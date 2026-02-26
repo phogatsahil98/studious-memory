b0cc7995a4b7bdbb6d2eaeb165c2f8c54c423f96
Sahil Phougat <109063865+phogatsahil98@users.noreply.github.com>
2026-02-26 22:11:01 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+//! Write a java program to segregate 1’s and 0’s
+/*
+public class Day2 {
+
+    public static void sorting0and1(ArrayList<Integer> arrayList){
+        // Using Bubble Sort
+
+        // Outer Loop
+        for (int i = 0; i < arrayList.size(); i++) {
+            // Inner Loop
+            for (int j = 0; j < arrayList.size()-i-1; j++) {
+                if (arrayList.get(j) > arrayList.get(j+1)){
+                    // Swap here
+                    int temp = arrayList.get(j);
+                    arrayList.set(j, arrayList.get(j+1));
+                    arrayList.set(j+1, temp);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(0);
+
+        sorting0and1(arrayList);
+        System.out.println("After Sorting 0 and 1 : "+arrayList);
+    }
+}
+ */
+//! Write a java program to search element in an array.
