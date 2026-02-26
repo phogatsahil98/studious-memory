4dc3ecf80a90877d8b979905044c7bea323753d3
Sahil Phougat <109063865+phogatsahil98@users.noreply.github.com>
2026-02-26 22:12:25 +0530

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,36 @@
+//! To broke string in some manner
+/*
+import java.util.StringTokenizer;
+public class Day4 {
+    public static void main(String[] args){
+        StringTokenizer st1, st2;
+
+        String quote1 = ("GOOG 604.45 -0.42");
+        st1 = new StringTokenizer(quote1);
+        System.out.println("Token 1 : "+st1.nextToken());
+        System.out.println("Token 2 : "+st1.nextToken());
+        System.out.println("Token 3 : "+st1.nextToken());
+
+        String quote2 = ("RHT@60.39@0.78");
+        st2 = new StringTokenizer(quote2, "@");
+        System.out.println("Token 1 : "+st2.nextToken());
+        System.out.println("Token 2 : "+st2.nextToken());
+        System.out.println("Token 3 : "+st2.nextToken());
+    }
+}
+ */
+
+//! Calculate Points based on inbuilt classes and objects
+/*
+import java.awt.Point;
+public class Day4 {
+    public static void main(String[] args){
+        Point pt = new Point(1, 2);
+        System.out.println(pt.x);
+        System.out.println(pt.getX());
+        System.out.println(pt.getY());
+        System.out.println(pt.getLocation().x);
+        System.out.println(pt.getLocation());
+    }
+}
+ */
